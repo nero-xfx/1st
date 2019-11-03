310fd44cdc34f6d54b3d051bb3ea97ece344660f
nero-xfx <57313996+nero-xfx@users.noreply.github.com>
2019-11-03 22:22:00 +0400

    create calculate

--- calculate.java
+++ calculate.java
@@ -1,6 +1,53 @@
 public class calculate {
-    public static void main(String[] args)
+    public static void main(String[] arg)
     {
         System.out.println("Calculate...");
+        String str="";
+        String[] parts=new String[2];
+        double result;
+        for(int i=0; i<arg.length; i++)
+        {str=str+arg[i];}
+        if (str.indexOf("+")>=0)
+        {
+        	System.out.print(str);
+            parts = str.split("\\+");
+            result = Double.valueOf(parts[0])+Double.valueOf(parts[1]);
+            System.out.print(" = "+result);
+        }
+        if (str.indexOf("-")>=0)
+        {
+        	System.out.print(str);
+            parts = str.split("-");
+            result = Double.valueOf(parts[0])-Double.valueOf(parts[1]);
+            System.out.print(" = "+result);
+        }
+        if (str.indexOf("*")>=0)
+        {   
+        	System.out.print(str);
+            parts = str.split("\\*");
+            result = Double.valueOf(parts[0])*Double.valueOf(parts[1]);
+            System.out.print(" = "+result);
+        }
+        if (str.indexOf("/")>=0)
+        {
+        	System.out.print(str);
+            parts = str.split("/");
+            result = Double.valueOf(parts[0])/Double.valueOf(parts[1]);
+            System.out.print(" = "+result);
+        }
+        if (str.indexOf(":")>=0)
+        {
+        	System.out.print(str);
+            parts = str.split(":");
+            result = Double.valueOf(parts[0])/Double.valueOf(parts[1]);
+            System.out.print(" = "+result);
+        }
+        if (str.indexOf("^")>=0)
+        {
+        	System.out.print(str);
+            parts = str.split("\\^");
+            result = Math.pow(Double.valueOf(parts[0]),Double.valueOf(parts[1]));
+            System.out.print(" = "+result);
+        }
     }
 }
